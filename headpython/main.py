bb7b5f8f08947ca2cb421c984b2d794afe590aa2
lcong <rockcong@gmail.com>
2017-08-03 19:54:01 +0800

    --

--- headpython/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from filePackage import myfile
-
-if __name__ == '__main__':
-    a = myfile("./filePackage/test.txt")
-    a.printFilePath();
-    a.testReadFile();
